f6d818b03300ee38943f852d03233c50f1fd9606
Anand S Kale <71403848+MrAk47Anand007@users.noreply.github.com>
2023-02-17 18:10:13 +0530

    Portfolio v0.3

--- Task4/index.js
+++ Task4/index.js
@@ -9,3 +9,38 @@ tabs.addEventListener("click",(e)=>{
         aboutSelection.querySelector(target).classList.add("active");
     }
 })
+
+/* Portfolio Popup*/
+
+document.addEventListener("click",(e)=>{
+    if (e.target.classList.contains("view-project-btn")){
+        togglePortfolioPopup();
+        document.querySelector(".portfolio-popup").scrollTo(0,0);
+        portfolioItemDetails(e.target.parentElement);
+    }
+})
+function togglePortfolioPopup(){
+    document.querySelector(".portfolio-popup").classList.toggle("open");
+    document.body.classList.toggle("hide-scrolling");
+    document.querySelector(".main").classList.toggle("fade-out");
+}
+document.querySelector(".pp-close").addEventListener("click",togglePortfolioPopup);
+document.addEventListener("click",(e)=>{
+    if (e.target.classList.contains("pp-inner")){
+        togglePortfolioPopup();
+    }
+})
+
+function portfolioItemDetails(portfolioItem){
+    document.querySelector(".pp-thumbnail img").src =
+        portfolioItem.querySelector(".portfolio-item-thumbnail img").src;
+
+    document.querySelector(".pp-header h3").innerHTML =
+        portfolioItem.querySelector(".portfolio-item-title").innerHTML;
+
+    document.querySelector(".pp-body .description").innerHTML=
+        portfolioItem.querySelector(".portfolio-item-details .description").innerHTML;
+
+    document.querySelector(".pp-body .general-info").innerHTML=
+        portfolioItem.querySelector(".portfolio-item-details .general-info").innerHTML;
+}
